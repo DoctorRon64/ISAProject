04b963fa5e7a84b95cac3199c630e330309392d3
DrRon <piepmail@lefthandmedia.com>
2023-06-08 01:56:01 +0200

    everything

---
+++ ISA school project/mods/stages/hku.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    makeLuaSprite('hku','stages/hku',-900, -620)
+    scaleObject('hku', 1.5, 1.5)
+    addLuaSprite('hku',false)
+    setProperty("girlfriendCameraOffset",{500, 0})
+end
